a2459b7f13b85741dc6b4045ed7cc8c8200cb326
whiteShark-one <2456922692@qq.com>
2026-07-23 22:45:49 +0800

    完成Section1：文本值和变量值：2-4单元

---
+++ Basics/Unit1OfTextAndVariables.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+    class Unit1OfTextAndVariables
+    {
+        // 成员变量
+        public string _firstname;
+        char _userOption;
+        int _gameScore;
+        decimal _particlePerMillion;
+        bool _processedCustomer;
+        public Unit1OfTextAndVariables(string firstname,char userOption,int gameScore,decimal particlePerMillion,bool processedCustomer)
+        {
+            _firstname = firstname;
+            _userOption = userOption;
+            _gameScore = gameScore;
+            _particlePerMillion = particlePerMillion;
+            _processedCustomer = processedCustomer;
+        }
+
+        // 成员函数
+
+        // 打印成员变量的值
+        public void PrintPro()
+        {
+            // 打印_firstname
+            Console.WriteLine("打印_firstname：{0}", _firstname);
+            // 打印_userOption
+            Console.WriteLine("打印_userOption：{0}", _userOption);
+        }
+
+        // 打印文本值
+        public void PrintText()
+        {
+            // 打印字符文本
+            Console.WriteLine("打印字符文本：{0}",'c');
+            // 打印整数文本
+            Console.WriteLine("打印整数文本：{0}",12);
+            // 打印浮点文本，要加字母f
+            Console.WriteLine("打印浮点文本：{0},{1}",0.25f,2.625f);
+            // 打印十进制文本，要加字母m
+            Console.WriteLine("打印十进制文本：{0}",12.39618m);
+            // 打印布尔文本
+            Console.WriteLine("打印布尔文本：{0}",true);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,11 +6,22 @@ namespace CSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
-            SolutionRectangle rectangle = new SolutionRectangle();
-            // rectangle.SayHello(); 
-            rectangle.Acceptdetails();
-            rectangle.Display();
+            // Console.WriteLine("Hello World");
+
+            // 菜鸟教程：基本语法
+            // SolutionRectangle rectangle = new SolutionRectangle();
+            // // rectangle.SayHello(); 
+            // rectangle.Acceptdetails();
+            // rectangle.Display();
+            
+            // 单元1：使用 C 中的文本值和变量值存储和检索数据
+            // 练习-打印文本值
+            Unit1OfTextAndVariables u = new Unit1OfTextAndVariables("Bob", 'm', 98, 12.235m, true);
+            // 打印类的成员属性
+            // Console.WriteLine(u.ToString);
+            u.PrintPro();
+            u.PrintText();
+            
         }
     }
 }
